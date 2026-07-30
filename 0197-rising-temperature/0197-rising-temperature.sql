6b7a814580f268dcfe4744bf007896426c326d57
Yogendra Tiwari <yogendra12tiwari@gmail.com>
2026-07-31 00:16:41 +0530

    Time: 216 ms (78.34%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+SELECT w1.id
+FROM Weather w1
+INNER JOIN Weather w2
+ON w1.recordDate = w2.recordDate + INTERVAL '1 day'
+WHERE w1.temperature > w2.temperature;
